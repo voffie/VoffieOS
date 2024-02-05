c9ce9f3fbbf7ec7962483993d28e5dcb28fdf4b3
VoffieDev <viktorsoderborg@gmail.com>
2024-02-05 18:48:52 +0100

    Update AboutModal

--- src/components/Modals/AboutModal.tsx
+++ src/components/Modals/AboutModal.tsx
@@ -1,4 +1,4 @@
-import { BaseModal } from "./BaseModal";
+import { BrowserModal } from "./BrowserModal";
 
 type Props = {
   isOpen: boolean;
@@ -7,19 +7,29 @@ type Props = {
 
 export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal
-      title="About me"
+    <BrowserModal
+      title="~/server/about-me.html"
       isOpen={isOpen}
       handleClose={handleClose}
     >
-      <p>
-        I got introduced to computers at an early age and have been interested
-        in them as far as I can remember. At first, it was only video games, but
-        as time passed I started to enjoy the more technical parts behind what
-        makes video games and computers work. This was especially true during my
-        last few years of education where I learned about coding, tools that
-        help developers with their work, and a few programming languages.
-      </p>
-    </BaseModal>
+      <section className="flex flex-col items-center p-2 h-full">
+        <img
+          src="images/selfie.JPG"
+          className="h-40 w-28 my-2"
+        />
+        <section>
+          <p>
+            Hey, my name is Viktor Söderborg also known as Voffie online. I'm a
+            Swedish web- & software developer and I got introduced to computers
+            at an early age and have been interested in them as far as I can
+            remember. At first, it was only video games, but as time passed I
+            started to enjoy the more technical parts behind what makes video
+            games and computers work. This was especially true during my last
+            few years of education where I learned about coding, tools that help
+            developers with their work, and a few programming languages.
+          </p>
+        </section>
+      </section>
+    </BrowserModal>
   );
 };
