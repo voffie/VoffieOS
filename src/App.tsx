33819a8ae2b7bf69d176ce0ea3da546c464c5609
VoffieDev <viktorsoderborg@gmail.com>
2024-02-29 18:28:06 +0100

    Add sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import { Desktop } from "./components/Desktop";
 import { Header } from "./components/Header";
+import { LoginWindow } from "./components/Window/LoginWindow";
 
 function App() {
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  setTimeout(() => setIsSignIn(false), 1750);
+
   return (
-    <Desktop>
-      <Header />
-    </Desktop>
+    <main className="fixed inset-0 overscroll-none">
+      {isSignIn && <LoginWindow />}
+      {!isSignIn && (
+        <div className="desktop h-full">
+          <Desktop>
+            <Header />
+          </Desktop>
+        </div>
+      )}
+    </main>
   );
 }
 
